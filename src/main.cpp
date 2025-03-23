6165b75e5ca83fa76e9582b25c0d2995f987f36b
Roma Golubev <romag643@gmail.com>
2025-03-23 11:35:53 +0300

    add demo program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char string[] = "Hello World!";
+    std::cout << faStr1(string) << std::endl;
     return 0;
 }
